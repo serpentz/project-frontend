7920aafbbd7e45e69ae6e0fee013f955c1c3252e
serpentz <absalomcherinet@gmail.com>
2018-11-10 14:49:57 -0500

    added basic card skeleton

---
+++ js/index.js
@@ -0,0 +1,61 @@
+ document.addEventListener("DOMContentLoaded" , () => {
+  console.log('done 1')
+    getCards()
+ })
+
+
+
+ function getCards(){
+ console.log('done 2')
+  //fetch
+  //data => render(data)
+
+  render(null)
+
+ }
+
+ function getList(){
+  return document.querySelector("#memes")
+ }
+
+ function render(data){
+   console.log('done 3')
+  let list = getList()
+  let pics = getPostsImageArray()
+
+    for( let x = 0 ; x < 5 ; x++){
+      let div = document.createElement("article")
+      div.className = "card"
+      div.innerHTML = new Post({picture:pics[x]}).render()
+
+      list.appendChild(div)
+    }
+
+ }
+
+ function getPostsImageArray(){
+    return ["https://i.ytimg.com/vi/a8c5wmeOL9o/maxresdefault.jpg",
+            "https://i.imgur.com/hXLIfXU.jpg",
+            "https://i.redd.it/yrun0mfr94u01.jpg",
+            "https://preview.redd.it/egewwisbnix11.jpg?width=640&crop=smart&auto=webp&s=20cd33f298eaaf477122eb222156b250f5b315ad"
+            ]
+ }
+
+
+
+
+
+
+  // <div id='card'>
+  //       <div id='identicon'>
+  //         <h1>Title</h1>
+  //       </div>
+
+  //       <!-- <form id='identicon-form'>
+  //         <div class='field'>
+  //           <input type='text' placeholder='Add Comment'/>
+  //           <input type='submit' class='btn' value="Generate" />
+  //         </div>
+  //       </form> -->
+
+  //     </div>
